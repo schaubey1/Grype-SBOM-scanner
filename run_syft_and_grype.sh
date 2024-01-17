3712ce4574b6fe03a83f93849f3b821b0de5ca4f
Sunny Chaubey <44815510+schaubey1@users.noreply.github.com>
2024-01-16 17:10:49 -0800

    Checkin grype-sbom-scanner

---
+++ run_syft_and_grype.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install Anchore Syft
+sudo curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b /usr/local/bin
+
+# Install Grype
+sudo curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sh -s -- -b /usr/local/bin
+# Create SBOM and run scan
+
+sudo syft / -o template -t csv.tmpl -o spdx-json=sbom.spdx.json
+
+grype --only-fixed sbom.spdx.json >> grype_report.txt
